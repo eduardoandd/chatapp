473f11ab0cb862059d16953eac94ee66044278c6
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-12-15 18:48:37 -0300

    aplicando configuração remota pelo database

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:chatapp/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(minutes: 1),
+    minimumFetchInterval: const Duration(hours: 1),
+  ));
+  // await remoteConfig.setDefaults(const {
+  //   "example_param_1": 42,
+  //   "example_param_2": 3.14159,
+  //   "example_param_3": true,
+  //   "example_param_4": "Hello, world!",
+  // });
+  await remoteConfig.fetchAndActivate();
+  print(remoteConfig.getString("example_param_1"));
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
